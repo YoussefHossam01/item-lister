465221ea7f4735237cd431c8e979c8074d6ac7f3
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2024-10-05 19:01:40 +0300

    Practice events and listeners

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-// Examine document object. //
+// Examine document object //
     // console.dir(document);
     // console.log(document.domain);
     // console.log(document.URL);
@@ -64,58 +64,118 @@
     // }
 
 // TRAVERSING DOM //
-    let itemlist = document.querySelector("#items")
-    // parentNode property
+    // let itemlist = document.querySelector("#items")
+    // parentNode property //
         // console.log(itemlist.parentNode);
         // itemlist.parentNode.style.backgroundColor ='grey';
         // console.log(items.parentNode.parentNode.parentNode);
-    // parentElement property
+    // parentElement property //
         // console.log(itemlist.parentElement);
         // itemlist.parentElement.style.backgroundColor ='grey';
         // console.log(items.parentElement.parentElement.parentElement);
-    // childNodes property and children property
+    // childNodes property and children property //
         // console.log(itemlist.childNodes)
         // console.log(itemlist.children);
         // console.log(itemlist.children[2]);
         // itemlist.children[2].style.backgroundColor = 'yellow';
-    // firstChild property kinda useless though
+    // firstChild property kinda useless though //
         // console.log(itemlist.firstChild)
-    //  firstElementChild
+    //  firstElementChild //
         // console.log(itemlist.firstElementChild)
-    //  lastElementChilde
+    //  lastElementChilde //
         // console.log(itemlist.lastElementChild)
-    //  lastChild kinda useless as well
+    //  lastChild kinda useless as well //
         // console.log(itemlist.lastChild)
-    // nextSibling property
+    // nextSibling property //
         // console.log(itemlist.nextSibling)
-    // nextElementSibling property
+    // nextElementSibling property //
         // console.log(itemlist.nextElementSibling)
-    // previousSibling property
+    // previousSibling property //
         // console.log(itemlist.previousSibling)
-    // previousElementSibling property
+    // previousElementSibling property //
         // console.log(itemlist.previousElementSibling)
 
-// createElement Method
-let newDiv= document.createElement('div');
-// Add class
-newDiv.className = 'hello';
-console.log(newDiv);
+// createElement Method //
+    // let newDiv= document.createElement('div');
+// Add class //
+    // newDiv.className = 'hello';
+    // console.log(newDiv);
 
-// Add id
-newDiv.id = "hi"
+// Add id //
+    // newDiv.id = "hi"
 
-// Add attribute
-newDiv.setAttribute('title', "Hello Div");
+// Add attribute //
+    // newDiv.setAttribute('title', "Hello Div");
 
-// Create text node
-let newDivText = document.createTextNode("Hello, World");
+// Create text node //
+    // let newDivText = document.createTextNode("Hello, World");
 
-// Add text to div
- newDiv.appendChild(newDivText);
+// Add text to div //
+    //  newDiv.appendChild(newDivText);
 
-// Insert div into the DOM
-let container = document.querySelector("header .container");
-let h1 = document.querySelector("header h1");
+// Insert div into the DOM //
+    // let container = document.querySelector("header .container");
+    // let h1 = document.querySelector("header h1");
 
-newDiv.style.fontSize = '30px';
-container.insertBefore(newDiv, h1);
+    // newDiv.style.fontSize = '30px';
+    // container.insertBefore(newDiv, h1);
+
+// EventListeners //
+
+// let button = document.querySelector("#btn").addEventListener
+            // ("click", buttonClick);
+
+// function buttonClick(e){
+    // document.getElementById("header-title").textContent = 'You Clicked!';
+    // document.querySelector("#main").style.backgroundColor = "lightgrey";
+    // console.log(e);
+    // console.log(e.target)
+    // console.log(e.target.id)
+    // console.log(e.target.className)
+    // console.log(e.type)
+    // console.log(e.clientX)
+    // console.log(e.clientY)
+    // console.log(e.offsetX)
+    // console.log(e.offsetY)
+    // console.log(e.altKey)
+    // console.log(e.ctrlKey)
+    // console.log(e.shiftKey)
+    // console.log(e.metaKey) 
+// }
+let button = document.querySelector("#btn");
+let box = document.querySelector("#box");
+// button.addEventListener('click', runEvent);
+// button.addEventListener('dblclick', runEvent);
+// button.addEventListener('mousedown', runEvent);
+// button.addEventListener('mouseup', runEvent);
+// box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseleave', runEvent);
+// box.addEventListener('mouseover', runEvent);
+// box.addEventListener('mouseout', runEvent);
+// box.addEventListener('mousemove', runEvent);
+
+// let itemInput = document.querySelector("input[type='text']")
+// let form = document.querySelector("form");
+// let select = document.querySelector("select")
+
+// itemInput.addEventListener("keydown", runEvent);
+// itemInput.addEventListener("keyup", runEvent);
+// itemInput.addEventListener("keypress", runEvent);
+// itemInput.addEventListener("focus", runEvent);
+// itemInput.addEventListener("blur", runEvent);
+// itemInput.addEventListener("cut", runEvent);
+// itemInput.addEventListener("paste", runEvent);
+// itemInput.addEventListener("input", runEvent);
+// select.addEventListener("change", runEvent)
+// select.addEventListener("input", runEvent)
+// form.addEventListener("submit", runEvent)
+
+// function runEvent(e){
+    // console.log("Event Type:" +" "+ e.type);
+    // e.preventDefault();
+    // console.log(e.target.value)
+    // console.log(e.offsetX + "  " + e.offsetY);
+    // box.style.backgroundColor = 'rgb('+e.offsetX+' , '+e.offsetY+', 40)';
+// }
+
+
